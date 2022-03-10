392c4bdcebe24233e673a1c0b71c5fd97e09493f
Daniel Luan Lourenço <danielluanlourencol@gmail.com>
2022-03-09 23:23:21 -0300

    dia 03 + desafio 1

---
+++ Aula 03/aula/script.js
@@ -0,0 +1,29 @@
+var numeroSecreto = parseInt(Math.random() * 11);
+var numTentativas = 3;
+console.log(numTentativas);
+
+function Chutar() {
+    numTentativas--;
+    document.getElementById("tentativas").innerHTML = "Tentativas restando: " + numTentativas;
+    console.log(numTentativas);
+    if (numTentativas < 1){
+        document.getElementById("tentativas").innerHTML = "Fim! Você perdeu suas tentativas! A resposta era " + numeroSecreto;
+        document.getElementById("resultado").innerHTML = " ";
+    }
+    else{
+        var numeroChute = parseInt(document.getElementById("valor").value);
+        var resultado = document.getElementById("resultado");
+
+        if (numeroChute == numeroSecreto) {
+            resultado.innerHTML = "Você acertou!"
+            document.getElementById("tentativas").innerHTML = " ";
+        }
+        else if (numeroChute > 10 || numeroChute < 0 ) {
+            resultado.innerHTML = "Digite um número entre 0 e 10!";
+            numTentativas++;
+        }
+        else {
+            resultado.innerHTML = "Você errou! Tente novamente!"
+    }
+    }
+}
