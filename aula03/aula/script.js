8fc142f2ce4574a0135bf924cbf486f8a3acbcce
Daniel Luan Lourenço <danielluanlourencol@gmail.com>
2022-03-13 15:57:59 -0300

    aula 3 correção + DANFLIX

--- aula03/aula/script.js
+++ aula03/aula/script.js
@@ -1,35 +1,34 @@
 var numeroSecreto = parseInt(Math.random() * 11);
 var numTentativas = 3;
+console.log(numeroSecreto);
 console.log(numTentativas);
 
 function Chutar() {
     numTentativas--;
     document.getElementById("tentativas").innerHTML = "Tentativas restando: " + numTentativas;
+    let numeroChute = document.getElementById("valor").value;
     console.log(numTentativas);
-    if (numTentativas < 1){
-        document.getElementById("tentativas").innerHTML = "Fim! Você perdeu suas tentativas! A resposta era " + numeroSecreto;
-        document.getElementById("resultado").innerHTML = " ";
-    }
-    else{
-        var numeroChute = parseInt(document.getElementById("valor").value);
-        var resultado = document.getElementById("resultado");
 
-        if (numeroChute == numeroSecreto) {
-            resultado.innerHTML = "Você acertou!"
-            document.getElementById("tentativas").innerHTML = " ";
-        }
-        else if (numeroChute > 10 || numeroChute < 0 ) {
-            resultado.innerHTML = "Digite um número entre 0 e 10!";
-            numTentativas++;
-            document.getElementById("tentativas").innerHTML = "Tentativas restando: " + numTentativas;
-        }
-        else {
-            if(numeroChute > numeroSecreto){
-                resultado.innerHTML = "Você errou! Tente novamente! O número é MENOR que " + numeroChute
-            }
-            else{
-                resultado.innerHTML = "Você errou! Tente novamente! O número é MAIOR que " + numeroChute
-            }
+    if(numeroSecreto == numeroChute){
+        resultado.innerHTML = "Você acertou!"
+        document.getElementById("tentativas").innerHTML = "";
     }
+    else if (numeroChute > 10 || numeroChute < 0 ) {
+        resultado.innerHTML = "Digite um número entre 0 e 10!";
+        numTentativas++;
+        document.getElementById("tentativas").innerHTML = "Tentativas restando: " + numTentativas;
     }
+    else if(numTentativas <= 0){
+        document.getElementById("tentativas").innerHTML = "Fim! Você perdeu suas tentativas! A resposta era " + numeroSecreto;
+        document.getElementById("resultado").innerHTML = "";
+    }
+    else {
+        if(numeroChute > numeroSecreto){
+            resultado.innerHTML = "Você errou! Tente novamente! O número é MENOR que " + numeroChute
+        }
+        else{
+            resultado.innerHTML = "Você errou! Tente novamente! O número é MAIOR que " + numeroChute
+        }
+    }
+        
 }
--- aula04/desafios/script.js
+++ aula04/desafios/script.js
@@ -10,19 +10,52 @@ var listaFilmes = [
 
 ];
 
+var nomeFilmes = [
+    "Interestelar",
+    "A Chegada",
+    "Ready Player One",
+    "Jarhead"
+];
+
 var listaElement = document.getElementById("catalogo-filmes");
 
 for(i = 0; i < listaFilmes.length; i++){
-    listaElement.innerHTML = listaElement.innerHTML + "<img src=" + listaFilmes[i] + " class=" + "catalogo-imagens" +">";
+    listaElement.innerHTML = listaElement.innerHTML + "<div class=" + "catalogo-filme-indiv" + " id=" + "catalogo-filme-indiv" + ">" + "<h3 class=" + "titulo-filme" + ">" + nomeFilmes[i] + "</h3>" + "<img src=" + listaFilmes[i] + " class=" + "catalogo-imagens" +">" + "</div>";
 }
 
+
 function addFilm() {
+    var newName = document.getElementById("entrada-nome").value;
     var newMovie = document.getElementById("entrada-texto").value;
-    
-    listaFilmes.push(newMovie);
 
-    console.log(listaFilmes[listaFilmes.length - 1]);
+    if(newMovie.endsWith(".jpg") || newMovie.endsWith(".png")){
+        listaElement.innerHTML = "";
+        listaFilmes.push(newMovie);
+        nomeFilmes.push(newName);
 
-        listaElement.innerHTML = listaElement.innerHTML + "<img src=" + listaFilmes[listaFilmes.length-1] + " class=" + "catalogo-imagens" +">";
+        for(i = 0; i < listaFilmes.length; i++){
+            listaElement.innerHTML = listaElement.innerHTML + "<div class=" + "catalogo-filme-indiv" + " id=" + "catalogo-filme-indiv" + ">" + "<h3 class=" + "titulo-filme" + ">" + nomeFilmes[i] + "</h3>" + "<img src=" + listaFilmes[i] + " class=" + "catalogo-imagens" +">" + "</div>";
+        }
+    }
+    else {
+        alert("Por favor, coloque o nome e o endereço de uma imagem válida!")
+    }
+    document.getElementById("entrada-texto").value = "";
+    document.getElementById("entrada-nome").value = "";
 
+}
+
+function removeFilm() {
+    let newName = document.getElementById("entrada-nome").value;
+    let indiceFilm = document.getElementById("entrada-indice").value;
+    console.log(newName);
+    console.log(indiceFilm);
+    let pos = nomeFilmes.indexOf(newName);
+
+    listaFilmes = listaFilmes.splice(pos);
+    nomeFilmes = nomeFilmes.splice(pos);
+
+    for(i = 0; i < listaFilmes.length; i++){
+        listaElement.innerHTML = listaElement.innerHTML + "<div class=" + "catalogo-filme-indiv" + " id=" + "catalogo-filme-indiv" + ">" + "<h3 class=" + "titulo-filme" + ">" + nomeFilmes[i] + "</h3>" + "<img src=" + listaFilmes[i] + " class=" + "catalogo-imagens" +">" + "</div>";
+    }
 }
